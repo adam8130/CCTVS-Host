1de2a71e3b5d55da757094c032d7373d3092e13b
chen <zzdx12345@gmail.com>
2023-03-07 00:44:23 +0800

    0.0.5

--- utils/getTaichung.js
+++ utils/getTaichung.js
@@ -8,19 +8,27 @@ const getTaichung = (targetURL, res) => {
     const protocol = targetURL.split('://')[0] === 'http' ? http : https
 
     const request = protocol.get(targetURL, (response) => {
-        let data = ''    
-        console.log(response.headers['content-type'])
-        res.writeHead(200, {
-            'access-control-allow-origin': '*',
-            'content-type': response.headers['content-type']
-        })
+        let data = ''
         response.on('data', (chunk) => {
             data += chunk
         })
         response.on('end', () => {
-            const src = srcReg.exec(data)[1]
-            res.end(src.replace('amp;', ''))
-            
+            const sourceURL = srcReg.exec(data)[1].replace('amp;', '')
+            const protocol = sourceURL.split('://')[0] === 'http' ? http : https
+            console.log(sourceURL)
+            protocol.get(sourceURL, (response2) => {
+                if (response2.statusCode === 200) {
+                    console.log(response2.headers['content-type'])
+                    res.writeHead(200, {
+                        'access-control-allow-origin': '*',
+                        'content-type': response2.headers['content-type']
+                    })
+                    response2.pipe(res)
+                }
+                else {
+                    res.end()
+                }
+            })
         })
     })
     request.on('err', (err) => {
---
+++ utils/proxyRequest.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const https = require('https')
+
+
+
+'YilanCounty'
+const proxyRequest = (requestURL, cityName, targetURL, res) => {
+
+    const edgeCase1 = ['Keelung', 'Taoyuan']
+    const edgeCase2 = ['ChanghuaCounty', 'Chiayi']
+    const protocol = targetURL.split('://')[0] === 'http' ? http : https
+    
+    if (edgeCase1.includes(cityName)) {
+        targetURL =  targetURL + '/'
+    }
+    if (edgeCase2.includes(cityName)) {
+        targetURL = requestURL.href.split('url=')[1]
+        console.log(targetURL)
+    }
+    const request = protocol.get(targetURL, (response) => {
+        console.log(response.headers['content-type'])
+        res.writeHead(200, {
+            'access-control-allow-origin': '*',
+            'content-type': response.headers['content-type'],
+        })
+        response.pipe(res)
+        response.on('error', (err) => {
+            console.log(err)
+        })
+    })
+    request.on('error', (err) => {
+        console.log(err)
+        request.end()
+    })
+}
+
+module.exports = proxyRequest
