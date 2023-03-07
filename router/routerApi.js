a9362bb3090494bb8f246b33ff30f92ada85093e
chen <zzdx12345@gmail.com>
2023-03-07 12:19:16 +0800

    0.0.6

--- router/routerApi.js
+++ router/routerApi.js
@@ -2,6 +2,9 @@ const http = require('http')
 const https = require('https')
 const getNewTaipei = require('../utils/getNewTaipei')
 const getTaichung = require('../utils/getTaichung')
+const getYilanCounty = require('../utils/getYilanCounty')
+const proxyRequest = require('../utils/proxyRequest')
+
 
 
 function routerApi (requestURL, res) {
@@ -17,41 +20,13 @@ function routerApi (requestURL, res) {
         case 'Taichung' :
             getTaichung(targetURL, res)
             return
+        case 'YilanCounty' :
+            getYilanCounty(requestURL, res)
+            return            
         default:
             proxyRequest(requestURL, cityName, targetURL, res)
             return
     }
 }
-
-const proxyRequest = (requestURL, cityName, targetURL, res) => {
-
-    const edgeCase1 = ['Keelung', 'Taoyuan']
-    const edgeCase2 = ['ChanghuaCounty', 'Chiayi']
-    const protocol = targetURL.split('://')[0] === 'http' ? http : https
-    
-    if (edgeCase1.includes(cityName)) {
-        targetURL =  targetURL + '/'
-    }
-    if (edgeCase2.includes(cityName)) {
-        targetURL = requestURL.href.split('url=')[1]
-        console.log(targetURL)
-    }
-    const request = protocol.get(targetURL, (response) => {
-        console.log(response.headers['content-type'])
-        res.writeHead(200, {
-            'access-control-allow-origin': '*',
-            'content-type': response.headers['content-type'],
-        })
-        response.pipe(res)
-        response.on('error', (err) => {
-            console.log(err)
-        })
-    })
-    request.on('error', (err) => {
-        console.log(err)
-        request.end()
-    })
-}
-
 
 module.exports = routerApi
